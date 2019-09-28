2127926ac78db4d5cab5a44eb1796468016bc898
Joacaldo <Joacaldo7965@users.noreply.github.com>
2019-09-28 20:21:35 -0300

    update
    mejora al musicPlayer y correcion de errores

--- musicPlayer.js
+++ musicPlayer.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
 var player;
+var player2;
 
 function startplayer(){
     player = document.getElementById('musicPlayer');
     player.controls = false;
+    player2 = document.getElementById('musicPlayer2');
+    player2.controls = false;
 }
 
 function play_aud(){
     player.play();
+    player2.pause();
 }
 function pause_aud(){
     player.pause();
@@ -18,4 +22,20 @@ function stop_aud(){
 }
 function changeVol(){
     player.volume = document.getElementById("changeVol").value;
+}
+
+
+function play_aud2(){
+    player2.play();
+    player.pause();
+}
+function pause_aud2(){
+    player2.pause();
+}
+function stop_aud2(){
+    player2.pause();
+    player2.currentTime = 0;
+}
+function changeVol2(){
+    player2.volume = document.getElementById("changeVol2").value;
 }
