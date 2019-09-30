1a0c4adae52114d02c1b459f5ed4fa52325f9a06
Joacaldo <Joacaldo7965@users.noreply.github.com>
2019-09-30 14:00:23 -0300

    update
    intente no dejar la caga

--- Creators.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(".fotos").hover(function(){
-      $("#elbody").css("background-color", "yellow");
-      }, function(){
-      $("#elbody").css("background-color", "pink");
-    });
-  });
